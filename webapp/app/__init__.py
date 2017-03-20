f75f3acb00e347ed443d4d06f61b009a03d9b7e5
Ben Hartshorne <ben@hound.sh>
2017-03-20 14:49:53 -0700

    adding very basic example honeycomb instrumentation

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import libhoney
 
 app = Flask(__name__)
+libhoney.init(writekey='abcabc123123', dataset='my flask app')
+
 
 from app import views
--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, jsonify
 from prometheus_client import Counter
 import requests
+import libhoney
 from app import app
 
 prom_counter = Counter('http_requests_total', 'Total HTTP Requests', ['method', 'endpoint', 'code'])
@@ -18,18 +19,24 @@ def index():
 
 @app.route('/get-repos', methods=["GET"])
 def get_repos():
+    ev = libhoney.Event()
     username = request.args.get("username")
+    ev.add_field("username",username)
 
     api_url = "https://api.github.com/users/{}/repos".format(username)
-    resp = requests.get(api_url)
+    with ev.timer("gitub_request_ms"):
+        resp = requests.get(api_url)
 
     label_dict = {"method": "GET",
                   "endpoint": "/get-repos",
                   "code": resp.status_code}
+    ev.add(label_dict)
 
     prom_counter.labels(**label_dict).inc()
 
     resp_dict = resp.json()
+    ev.add_field("num_repos", len(resp_dict))
+    ev.send()
 
     repos = [x["name"] for x in resp_dict]
 
